62f021b403f673b4090247e45899ba53e4ef7039
Shannon Sullivan <shsulliv@gmail.com>
2017-02-07 14:19:40 +0100

    Working version that accepts user's location - has preliminary styling

--- app.js
+++ app.js
@@ -2,20 +2,199 @@
 // https://developers.google.com/maps/documentation/javascript/adding-a-google-map
 // https://www.sitepoint.com/find-a-route-using-the-geolocation-and-the-google-maps-api/
 
+// Nautical styled map from: https://snazzymaps.com/style/92258/nautical-map
+const nauticalStyle =
+[
+    {
+        "featureType": "administrative",
+        "elementType": "labels.text.fill",
+        "stylers": [
+            {
+                "color": "#101f4b"
+            }
+        ]
+    },
+    {
+        "featureType": "administrative",
+        "elementType": "labels.text.stroke",
+        "stylers": [
+            {
+                "color": "#ffffff"
+            }
+        ]
+    },
+    {
+        "featureType": "landscape",
+        "elementType": "all",
+        "stylers": [
+            {
+                "color": "#ffffff"
+            }
+        ]
+    },
+    {
+        "featureType": "landscape",
+        "elementType": "labels.text",
+        "stylers": [
+            {
+                "color": "#ff0000"
+            },
+            {
+                "visibility": "off"
+            }
+        ]
+    },
+    {
+        "featureType": "poi",
+        "elementType": "all",
+        "stylers": [
+            {
+                "visibility": "off"
+            }
+        ]
+    },
+    {
+        "featureType": "road",
+        "elementType": "all",
+        "stylers": [
+            {
+                "saturation": -100
+            },
+            {
+                "lightness": 45
+            },
+            {
+                "color": "#f4f4f4"
+            },
+            {
+                "visibility": "simplified"
+            }
+        ]
+    },
+    {
+        "featureType": "road",
+        "elementType": "labels.text",
+        "stylers": [
+            {
+                "color": "#101f4b"
+            }
+        ]
+    },
+    {
+        "featureType": "road.highway",
+        "elementType": "all",
+        "stylers": [
+            {
+                "visibility": "simplified"
+            }
+        ]
+    },
+    {
+        "featureType": "road.highway",
+        "elementType": "geometry.fill",
+        "stylers": [
+            {
+                "saturation": "23"
+            },
+            {
+                "color": "#d42039"
+            }
+        ]
+    },
+    {
+        "featureType": "road.highway",
+        "elementType": "labels.text.fill",
+        "stylers": [
+            {
+                "color": "#ffffff"
+            }
+        ]
+    },
+    {
+        "featureType": "road.highway",
+        "elementType": "labels.icon",
+        "stylers": [
+            {
+                "visibility": "off"
+            }
+        ]
+    },
+    {
+        "featureType": "road.arterial",
+        "elementType": "labels.icon",
+        "stylers": [
+            {
+                "visibility": "off"
+            }
+        ]
+    },
+    {
+        "featureType": "road.local",
+        "elementType": "geometry",
+        "stylers": [
+            {
+                "saturation": "-19"
+            },
+            {
+                "visibility": "simplified"
+            }
+        ]
+    },
+    {
+        "featureType": "transit",
+        "elementType": "all",
+        "stylers": [
+            {
+                "visibility": "off"
+            }
+        ]
+    },
+    {
+        "featureType": "water",
+        "elementType": "all",
+        "stylers": [
+            {
+                "color": "#d9eded"
+            },
+            {
+                "visibility": "on"
+            }
+        ]
+    }
+]
+
 const calculateRoute = ({ travelMode }) => {
 
+  // Building the Map
   const mapOptions = {
     zoom: 10,
     center: {lat: 51.5074, lng: 0.1278}, // Center initialized to London
-    mapTypeId: google.maps.MapTypeId.ROADMAP
+    mapTypeId: google.maps.MapTypeId.ROADMAP,
+    mapTypeControl: false,
+    styles: nauticalStyle
   };
+
+  // Try HTML5 geolocation.
+   if (navigator.geolocation) {
+     navigator.geolocation.getCurrentPosition((position) => {
+       var pos = {
+         lat: position.coords.latitude,
+         lng: position.coords.longitude
+       };
+       mapObject.setCenter(pos);
+     });
+   } else {
+     console.log('No support for geolocation');
+   }
 
   const mapObject =
     new google.maps.Map(document.getElementById('map'), mapOptions);
 
+  // Accessing the Directions API
   const directionsService = new google.maps.DirectionsService();
 
-  const start = {lat: 51.5138453, lng: -0.1005393}; // St. Paul's Cathedral
+  // const start = {lat: 51.5138453, lng: -0.1005393}; // St. Paul's Cathedral
+  const start = mapObject.getCenter();
   const end = {lat: 51.5332408, lng: -0.1281903}; // Google London, King's Cross
 
   const directionsRequest = {
@@ -40,6 +219,7 @@ const calculateRoute = ({ travelMode }) => {
   );
 };
 
+// Setting the buttons to change mode of transportation
 const controls = document.getElementById('controls');
 
 controls.addEventListener('click', (event) => {
@@ -47,4 +227,5 @@ controls.addEventListener('click', (event) => {
   calculateRoute({travelMode: event.target.value});
 });
 
+// Setting an initialized route to 'Driving' on page load
 calculateRoute({ travelMode: 'DRIVING' });
