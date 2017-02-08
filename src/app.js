6204effbbd35ede49cf3d718e8903a295e948773
Shannon Sullivan <shsulliv@gmail.com>
2017-02-08 23:24:57 +0100

    Removed commented out old code

--- src/app.js
+++ src/app.js
@@ -3,19 +3,6 @@
 // https://www.sitepoint.com/find-a-route-using-the-geolocation-and-the-google-maps-api/
 // Nautical styled map from: https://snazzymaps.com/style/92258/nautical-map
 
-// const calculateRoute = require('./route');
-//
-// // Setting the buttons to change mode of transportation
-// const controls = document.getElementById('controls');
-//
-// controls.addEventListener('click', event => {
-//   event.preventDefault();
-//   calculateRoute({ travelMethod: event.target.parentNode.value });
-// });
-
-// Setting an initialized route to 'Driving' on page load
-// calculateRoute({ travelMethod: 'DRIVING' });
-
 const { coordinates, MAP_OPTIONS } = require('./constants');
 const { makeMap, renderMarker } = require('./map');
 const geolocate = require('./geolocation');
